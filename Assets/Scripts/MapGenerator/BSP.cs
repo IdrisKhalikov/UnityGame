fec959790e67fa5b11f56c300ce0f6abf4ce9e08
NeonyFenyx <idrishal277@gmail.com>
2023-04-19 02:11:32 +0500

    Added simple level generation, not finished yet

---
+++ Assets/Scripts/MapGenerator/BSP.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BSPTreeGeneration
+{
+    public class Partition
+    {
+        public Partition Neighbour { get; private set; }
+        public readonly Rectangle Bounds;
+        public readonly int Iteration;
+
+        public Partition(Rectangle bounds, int iteration)
+        {
+            Bounds = bounds;
+            Iteration = iteration;
+        }
+
+        public void SetNeighbour(Partition other)
+        {
+            Neighbour = other;
+            other.Neighbour = this;
+        }
+    }
+
+    public class BSPAlgorithm
+    {
+        private int width;
+        private int height;
+        const double MaxRatio = 0.65;
+        const double MinRatio = 0.45;
+        private Random random;
+
+        public BSPAlgorithm(int levelWidth, int levelHeight)
+        {
+            width = levelWidth;
+            height = levelHeight;
+            random = new Random();
+        }
+
+        public List<Partition> GetMap(int iterations)
+        {
+            var partitions = new List<Partition>();
+            var stack = new Stack<Partition>();
+            var initialBounds = new Rectangle(0, 0, width, height);
+            stack.Push(new Partition(initialBounds, 0));
+            while(stack.Count > 0)
+            {
+                var partition = stack.Pop();
+                partitions.Add(partition);
+                var splitted = Split(partition);
+                if (partition.Iteration == iterations)    
+                    continue;
+
+                stack.Push(splitted.Item1);
+                stack.Push(splitted.Item2);
+            }
+
+            return partitions;
+        }
+
+        public (Partition, Partition) Split(Partition partition)
+        {
+            var pair = SplitRect(partition.Bounds);
+            var firstPart = new Partition(pair.Item1, partition.Iteration + 1);
+            var secondPart = new Partition(pair.Item2, partition.Iteration + 1);
+            firstPart.SetNeighbour(secondPart);
+            return (firstPart, secondPart);
+        }
+
+        private (Rectangle, Rectangle) SplitRect(Rectangle rect)
+        {
+            var split = random.NextDouble() * (MaxRatio - MinRatio) + MinRatio;
+            if (rect.Height > rect.Width)
+            {
+                var r1 = new Rectangle(rect.X, rect.Y, rect.Width, (int)(rect.Height * split));
+                var r2 = new Rectangle(rect.X, rect.Y + r1.Height, rect.Width, rect.Height - r1.Height);
+                return (r1, r2);
+            }
+            else
+            {
+                var r1 = new Rectangle(rect.X, rect.Y, (int)(rect.Width * split), rect.Height);
+                var r2 = new Rectangle(rect.X + r1.Width, rect.Y, rect.Width - r1.Width, rect.Height);
+                return (r1, r2);
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/MapGenerator/MapGenerator.cs
@@ -0,0 +1,97 @@
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Linq;
+using Unity.VisualScripting;
+using System.Threading;
+
+namespace BSPTreeGeneration
+{
+    public class MapGenerator
+    {
+        private Tilemap tilemap;
+        private Tile wall;
+        private Tile floor;
+
+        public MapGenerator(Tilemap tilemap, Tile wall, Tile floor)
+        {
+            this.tilemap = tilemap;
+            this.wall = wall;
+            this.floor = null;
+        }
+
+        public void GenerateMap(int levelWidth, int levelHeight, int iterations)
+        {
+            tilemap.ClearAllTiles();
+            tilemap.size = new Vector3Int(levelWidth, levelHeight);
+            tilemap.ResizeBounds();
+            tilemap.SetTile(new Vector3Int(levelWidth + 1, levelHeight + 1), floor);
+            var partitions = new BSPAlgorithm(levelWidth, levelHeight).GetMap(iterations);
+            foreach(var partition in partitions)
+            {
+                if(partition.Neighbour != null)
+                    DrawCorridor(partition);
+            }
+
+            foreach(var partition in partitions)
+            {
+                if (partition.Iteration == iterations)
+                    FillRect(GenerateRoom(partition.Bounds));
+            }
+
+            Debug.Log("OK");
+        }
+
+        private static Rectangle GenerateRoom(Rectangle rect)
+        {
+            var newX = rect.X + Random.Range(1, rect.Width / 3);
+            var newY = rect.Y + Random.Range(1, rect.Height / 3);
+            var width = rect.Width - (newX - rect.X);
+            var height = rect.Height - (newY - rect.Y);
+            width -= Random.Range(1, width / 3);
+            height -= Random.Range(1, height / 3);
+            return new Rectangle(newX, newY, width, height);
+        }
+
+        private void DrawCorridor(Partition partition)
+        {
+            var start = GetRectangleCenter(partition.Bounds);
+            var end = GetRectangleCenter(partition.Neighbour.Bounds);
+            if (start.X == end.X)
+            {
+                FillRect(new Point(start.X - 2, start.Y - 2), new Point(end.X + 1, end.Y + 2));
+            }
+            else
+            {
+                FillRect(new Point(start.X - 2, start.Y - 2), new Point(end.X + 2, end.Y + 1));
+            }
+        }
+
+        private void DrawRect(Rectangle rect, Tile tile)
+        {
+            DrawRect(new Point(rect.X, rect.Y), new Point(rect.X + rect.Width, rect.Y + rect.Height), tile);
+        }
+
+        private void DrawRect(Point start, Point end, Tile tile)
+        {
+            for (var x = start.X; x <= end.X; x++)
+                for (var y = start.Y; y <= end.Y; y++)
+                    tilemap.SetTile(new Vector3Int(x, y), tile);
+        }
+
+        private void FillRect(Point start, Point end)
+        {
+            DrawRect(start, end, wall);
+            DrawRect(new Point(start.X + 1, start.Y + 1), new Point(end.X - 1, end.Y - 1), floor);
+        }
+
+        private void FillRect(Rectangle rect)
+        {
+            DrawRect(rect, wall);
+            DrawRect(new Rectangle(rect.X + 1, rect.Y + 1, rect.Width - 2, rect.Height - 2), floor);
+        }
+
+        private static Point GetRectangleCenter(Rectangle rect)
+            => new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+    }
+}
